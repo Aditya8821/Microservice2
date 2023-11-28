864c8b87ed128d072b405837e3ac3dae0ac752de
adityaofficial88 <144439788+adityaofficial88@users.noreply.github.com>
2023-11-28 14:36:01 +0530

    Update price_api.py

--- price_api.py
+++ price_api.py
@@ -1,4 +1,5 @@
-# from flask import Flask, render_template, request, jsonify
+# This is for ENV variables
+# from flask import Flask, render_template, request, jsonify  
 # import requests
 # import os
 
@@ -32,6 +33,45 @@
 #     app.run(debug=True, host='0.0.0.0', port=5001)
 
 
+
+
+
+
+
+# This is the original first one
+# from flask import Flask, render_template, request, jsonify
+# import requests
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/calculateprice', methods=['POST'])
+# def calculate_price():
+#     product_id = request.form['product_id']
+#     quantity = int(request.form['quantity'])
+
+#     # Make a request to the first API to get product details
+#     api1_url = 'http://localhost:5000/getprice/' + product_id  # Replace with the actual IP or domain
+#     response = requests.get(api1_url)
+
+#     if response.status_code == 200:
+#         price = response.json().get('price')
+#         total_price = price * quantity
+#         return render_template('result.html',total_price=total_price)
+#     else:
+#         return render_template('result.html', error="Failed to fetch product details from API1")
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
+
+
+
+
+
+
 from flask import Flask, render_template, request, jsonify
 import requests
 
@@ -48,14 +88,15 @@ def calculate_price():
 
     # Make a request to the first API to get product details
     api1_url = 'http://localhost:5000/getprice/' + product_id  # Replace with the actual IP or domain
-    response = requests.get(api1_url)
-
-    if response.status_code == 200:
+    try:
+        response = requests.get(api1_url)
+        response.raise_for_status()  # Raise an exception for bad responses
         price = response.json().get('price')
         total_price = price * quantity
-        return render_template('result.html',total_price=total_price)
-    else:
-        return render_template('result.html', error="Failed to fetch product details from API1")
+        return render_template('result.html', total_price=total_price)
+    except requests.exceptions.RequestException as e:
+        return render_template('result.html', error=f"Failed to fetch product details from API1: {e}")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
