e60d90d340badb129ada296d07cf064a43404a0a
adityaofficial88 <144439788+adityaofficial88@users.noreply.github.com>
2023-11-29 21:38:35 +0530

    Update price_api.py

--- price_api.py
+++ price_api.py
@@ -1,3 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculateprice', methods=['POST'])
+def calculate_price():
+    product_id = request.form['product_id']
+    quantity = int(request.form['quantity'])
+
+    # Make a request to the first API to get product details
+    api1_url = 'http://172.31.88.214:5000/getprice/' + product_id  # Replace with the actual IP or domain or you can try by giving private IP of instance also
+    try:
+        response = requests.get(api1_url)
+        response.raise_for_status()  # Raise an exception for bad responses
+        price = response.json().get('price')
+        total_price = price * quantity
+        return render_template('result.html', total_price=total_price)
+    except requests.exceptions.RequestException as e:
+        return render_template('result.html', error=f"Failed to fetch product details from API1: {e}")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
+
+
+
+
+
+
+
+
 # This is for ENV variables
 # from flask import Flask, render_template, request, jsonify  
 # import requests
@@ -72,31 +108,5 @@
 
 
 
-from flask import Flask, render_template, request, jsonify
-import requests
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/calculateprice', methods=['POST'])
-def calculate_price():
-    product_id = request.form['product_id']
-    quantity = int(request.form['quantity'])
-
-    # Make a request to the first API to get product details
-    api1_url = 'http://172.31.88.214:5000/getprice/' + product_id  # Replace with the actual IP or domain
-    try:
-        response = requests.get(api1_url)
-        response.raise_for_status()  # Raise an exception for bad responses
-        price = response.json().get('price')
-        total_price = price * quantity
-        return render_template('result.html', total_price=total_price)
-    except requests.exceptions.RequestException as e:
-        return render_template('result.html', error=f"Failed to fetch product details from API1: {e}")
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
 
